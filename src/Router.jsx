40991f051fea059c5b8247e0dda2f39464c9988f
Kavit Shah <kavitshah8@gmail.com>
2016-10-29 07:22:29 -0700

    add eslint for static analysis of the code

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { browserHistory, Route, Router } from 'react-router';
 
-import HelloWorld from './components/HelloWorld.jsx'
-import Styles from './styles/app.css';
+import HelloWorld from './components/HelloWorld.jsx';
+// import Styles from './styles/app.css';
 
 render((
     <Router history={browserHistory}>
